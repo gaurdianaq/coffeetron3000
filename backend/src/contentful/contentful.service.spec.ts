2072cba7bdf764fb624d98bca251a41c11c48d15
Evan Tatay-Hinds <evan.tatay-hinds@applydigital.co>
2023-10-03 11:49:32 -0400

    Accidentally committed some old code

--- backend/src/contentful/contentful.service.spec.ts
+++ backend/src/contentful/contentful.service.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ContentfulService } from "./contentful.service";
+import { EntrySkeletonType } from "contentful";
 
 describe("ContentfulService", () => {
-  let service: ContentfulService;
+  let service: ContentfulService<EntrySkeletonType, undefined>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ContentfulService],
     }).compile();
 
-    service = module.get<ContentfulService>(ContentfulService);
+    service =
+      module.get<ContentfulService<EntrySkeletonType, undefined>>(
+        ContentfulService
+      );
   });
 
   it("should be defined", () => {
